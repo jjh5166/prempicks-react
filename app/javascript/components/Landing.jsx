3b55b2c0dbacbf99e84f3cf396c88ba45d34f17b
John <jjh5166@gmail.com>
2020-08-17 15:07:53 -0400

    landing page styles, button extend css

--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default () => (
-  <div className="page_container">
+  <div className="page_container hero_img">
     <h1 className="home_header">Prem Picks</h1>
     <div className="landing_buttons">
       <a className="btn landing-btn">Log In</a>
