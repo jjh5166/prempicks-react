70f2d96478ca4a0961a34f1fd9d55cb09aa3c336
John <jjh5166@gmail.com>
2020-08-17 18:04:59 -0400

    TxtField component

---
+++ app/javascript/components/forms/TxtField.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useField } from 'formik';
+import { TextField } from '@material-ui/core';
+
+const TxtField = ({ placeholder, isPassword = false, ...props }) => {
+  const [field, meta] = useField(props);
+  const errorText = meta.error && meta.touched ? meta.error : '';
+  const password = isPassword ? "password" : "";
+
+  return (
+    <TextField
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+      placeholder={placeholder}
+      type={password}
+    />
+  );
+};
+
+
+export default TxtField;
