03eb5d3394f989a08ce07aadec043d1b6d6f71b5
John <jjh5166@gmail.com>
2020-08-17 13:26:14 -0400

    rm old routes, match for react routing

--- config/routes.rb
+++ config/routes.rb
@@ -17,25 +17,8 @@ Rails.application.routes.draw do
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
-  authenticated :user do
-    root to: 'picks#mypicks', as: :authenticated_root
-  end
+
   root to: 'landing#index'
-  get '/*path' => 'landing#index'
+  match '/*path' => 'landing#index', via: :all
 
-  get '/rules' => 'static_pages#rules', as: :rules
-  get '/table' => 'epldata#table', as: :epl_table
-  get '/schedule/:matchday' => 'epldata#schedule', as: :schedule
-  get '/standings' => 'picks#standings', as: :standings
-  get '/mypicks' => 'picks#mypicks', as: :mypicks
-
-  #Guest Views
-  get '/welcome' => 'static_pages#guest_welcome', as: :welcome
-  get '/mypicks/g' => 'picks#guest_mypicks', as: :gpicks
-  get '/standings/g' => 'picks#guest_standings', as: :gstandings
-
-  devise_scope :user do
-    patch '/mypicks_update' => 'users/update_picks#update_picks', :as => :update_picks
-    patch '/gpicks_update' => 'users/update_picks#update_guest_picks', :as => :update_guest_picks
-  end
 end
