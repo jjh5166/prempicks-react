48ca7edfadc01139f818dc0e3f543b13f8c5b39d
John <jjh5166@gmail.com>
2020-08-17 15:17:12 -0400

    login, signup routes

--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -4,8 +4,8 @@ export default () => (
   <div className="page_container hero_img">
     <h1 className="home_header">Prem Picks</h1>
     <div className="landing_buttons">
-      <a className="btn landing-btn">Log In</a>
-      <a className="btn landing-btn">Sign Up</a>
+      <a className="btn landing-btn" href="/login">Log In</a>
+      <a className="btn landing-btn" href="/signup">Sign Up</a>
     </div>
   </div>
 );
