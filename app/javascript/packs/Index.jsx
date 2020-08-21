127d65bb4aa2fd4ce2b0a6f80e855dba1dadccc8
John <jjh5166@gmail.com>
2020-08-20 21:41:11 -0400

    add basic bootstrap, import css

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('root'));
