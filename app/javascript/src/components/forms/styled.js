4b061539868e022fb2ddebeda3be109b8b98543c
John <jjh5166@gmail.com>
2020-08-20 20:43:00 -0400

    FormContainer

---
+++ app/javascript/src/components/forms/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 50%;
+  text-align: center;
+  border-radius: 10px;
+  background: linear-gradient(90deg, rgba(31,89,47,1) 0%, rgb(41, 161, 111) 35%, rgb(8, 78, 31) 100%);
+  padding-bottom: 10px;
+  border: 2px groove grey;
+  color: rgba(231, 241, 241, 1);
+  text-shadow: 1px 1px #00394d;
+  overflow-y: scroll;
+`
