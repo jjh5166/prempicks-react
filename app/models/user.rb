978a1c6d976f98d37673b2dd60637c3e0c40dede
John <jjh5166@gmail.com>
2020-08-22 21:23:15 -0400

    rails login validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
   has_many :picks, dependent: :destroy, inverse_of: :user
   has_many :guest_picks, dependent: :destroy, inverse_of: :user
   accepts_nested_attributes_for :picks, :guest_picks
   validates_associated :picks, :guest_picks
   validate :password_complexity
+  validates :team_name, uniqueness: {message: "Sorry, this team name is taken"}
+  validates :email, uniqueness: {message: "This email already in use"}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please enter a valid email" } 
   after_create :send_sign_up_emails, :seed_picks
 
   private
@@ -14,7 +17,7 @@ class User < ApplicationRecord
     # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
 
-    errors.add :password, 'Complexity requirement not met. Length should be at least 8 and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, 'Password should be at least 8 characters long and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
 
   def send_sign_up_emails
