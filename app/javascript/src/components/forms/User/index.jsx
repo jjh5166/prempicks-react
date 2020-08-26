1b8e26b41b5b45c163bfa3ccd8f92ceb268efe17
John <jjh5166@gmail.com>
2020-08-26 17:25:54 -0400

    UserForm, FormFields, TxtField def exp

---
+++ app/javascript/src/components/forms/User/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Formik, Form } from 'formik';
+
+import { FormContainer } from './styled';
+import Layout from '../../Layout';
+import FormFields from '../elements/FormFields';
+import Bttn from '../elements/Bttn';
+import { ToastsContainer, ErrorDisplay } from '../elements/Toasts';
+
+const UserForm = ({ name, initialValues, validationSchema, submitFn, fields }) => {
+  const [errors, setErrors] = useState(null);
+  return (
+    <Layout>
+      <ToastsContainer>
+        {errors && <ErrorDisplay errors={errors} />}
+      </ToastsContainer>
+      <FormContainer>
+        <h2>{name}</h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(data, { setSubmitting }) => {
+            setSubmitting(true);
+            submitFn(data, setErrors);
+            setSubmitting(false);
+          }}
+        >
+          {({ dirty, isValid, isSubmitting }) => (
+            <Form>
+              <FormFields fields={fields} />
+              <Bttn text={name} disabled={!dirty || !isValid || isSubmitting} />
+            </Form>
+          )}
+        </Formik>
+      </FormContainer>
+    </Layout>
+  );
+};
+
+export default UserForm;
